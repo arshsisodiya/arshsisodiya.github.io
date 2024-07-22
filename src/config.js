d8057d501f546f51bd58f8fc252069d9cce2edfc
arshsisodiya <arshsisodiya@gmail.com>
2024-07-22 16:33:56 +0530

    added social media links and email

--- src/config.js
+++ src/config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: 'arshsisodiya@gmail.com',
 
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/arshsisodiya',
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
+      url: 'https://www.instagram.com/arsh__sisodiya',
     },
     {
-      name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      name: 'X',
+      url: 'https://x.com/arshsisodiya',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/bhagchand-meena-b54712117/',
     },
     {
-      name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
+      name: 'Telegram',
+      url: 'https://t.me/arshsisodiya',
     },
   ],
 
