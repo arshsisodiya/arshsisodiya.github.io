d708e52671646f1ee0054a3d62e411171e28da9d
arshsisodiya <arshsisodiya@gmail.com>
2024-07-22 15:54:32 +0530

    updated my details and added links to profiles

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,28 +60,35 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
+  const two = <h2 className="big-heading">Arsh Sisodiya.</h2>;
   const three = <h3 className="big-heading">I build things for the web.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        My real name is Bhagchand Meena, I’m a computer science student and programmer. Outside of
+        work and studies, I enjoy writing short, amateur pieces on{' '}
+        <a href="https://instagram.com/arsh__sisodiya" target="_blank" rel="noreferrer">
+          Instagram
+        </a>{' '}
+        and sharing my tech adventures on{' '}
+        <a href="https://x.com/arshsisodiya" target="_blank" rel="noreferrer">
+          X
         </a>
-        .
+        . When I’m not in front of a screen, you’ll find me immersed in a good book,{' '}
+        <a href="https://t.me/arshxplaylist" target="_blank" rel="noreferrer">
+          listening to music
+        </a>{' '}
+        or playing video games.
       </p>
     </>
   );
   const five = (
     <a
       className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
+      href="https://www.github.com/arshsisodiya"
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      My Github
     </a>
   );
 
