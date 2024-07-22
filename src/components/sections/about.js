dfd9478d50704aee260abfeecfb174936aba54d8
arshsisodiya <arshsisodiya@gmail.com>
2024-07-22 15:33:49 +0530

    updated to new version
    added files and changed about me section

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,8 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['Python', 'Javascript', 'Java', 'Linux', 'Adobe Premier Pro', 'Docker'];
+  const service = ['Heroku', 'Netlify', 'Azure', 'Google Cloud'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,37 +136,43 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Bhagchand Meena aka Arsh Sisodiya and I enjoy creating things that
+              live on the internet. I have a background in Science-Maths from high school and
+              started programming in 2019. and completed my Bachelors in
+              <a href="https://poornima.edu.in">Computer Applicatons </a> in 2021 with 86.3%
+              aggregate.<br></br>
+              Currently, I am pursuing my master’s in the same field.
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              I’ve developed{' '}
+              <a href="https://github.com/arshsisodiya/helios-mirror/">
+                a Telegram Mirror and Leech bot
+              </a>
+              ,{' '}
+              <a href="https://github.com/arshsisodiya/ProjectFizilion">
+                a Telegram userbot for automating tasks
+              </a>
+              , and I've also worked with the custom ROM community to build and release custom ROMs
+              for android phones, along with many web apps. Additionally, I have built a community
+              of almost 2,000 people on Telegram, called{' '}
+              <a href="https://t.me/mirrorsociety1"> Mirror Society</a>, to share ideas and help
+              each other.
             </p>
 
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
-            </p>
-
-            <p>Here are a few technologies I’ve been working with recently:</p>
+            <p>Here are a few technologies and apps I’ve been working with recently:</p>
           </div>
 
           <ul className="skills-list">
             {skills && skills.map((skill, i) => <li key={i}>{skill}</li>)}
+          </ul>
+          <div>
+            <p></p>
+            <p>Here are a few cloud services I have used:</p>
+          </div>
+
+          <ul className="skills-list">
+            {service && service.map((skill, i) => <li key={i}>{skill}</li>)}
           </ul>
         </StyledText>
 
