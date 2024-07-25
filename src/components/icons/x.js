2defb720f45c4aca37d5b7d5a46652361fb1cbef
arshsisodiya <arshsisodiya@gmail.com>
2024-07-25 22:57:09 +0530

    feat: Update Telegram and X.com icons to use FontAwesome
    - Replaced existing Telegram and X.com icons with FontAwesome icons for consistency and improved design.

--- src/components/icons/x.js
+++ src/components/icons/x.js
@@ -1,18 +1,8 @@
 import React from 'react';
 
 const IconX = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    role="img"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    className="feather feather-x">
-    <title>X.com</title>
-    <path d="M18 2L2 22M2 2l16 20"></path>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+    <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
   </svg>
 );
 
